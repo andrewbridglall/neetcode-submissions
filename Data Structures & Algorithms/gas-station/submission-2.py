9626bd08c6d2cbe0ff34427ff6f7b941f562180f
Andrew Bridglall <66570568+andrewbridglall@users.noreply.github.com>
2026-04-06 12:26:17 -0400

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # optimal - single pass
+        # init starting pos
+        n = len(gas)
+        start = 0
+        startRaw = 0
+        # traverse
+        while True:
+            tank = 0
+            dest = start
+            destRaw = startRaw
+            while True:
+                tank += gas[dest]
+                tank -= cost[dest]
+                if tank >= 0:
+                    # update dest
+                    dest = (dest+1) % n
+                    destRaw = destRaw + 1
+                else:
+                    # fails to make it to next index
+                    start = (dest+1) % n
+                    startRaw = destRaw + 1
+                    break
+                if dest == start:
+                    return start
+            if startRaw >= n:
+                return -1
+        # update start if fails
+        # ret -1 if no soln
+        
