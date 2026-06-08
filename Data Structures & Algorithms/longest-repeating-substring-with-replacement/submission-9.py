a445c150a9a449c03cf74d7641d7465a3ef33468
Andrew Bridglall <66570568+andrewbridglall@users.noreply.github.com>
2026-06-08 14:07:41 -0400

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # bf soln
+        # init vars
+        n = len(s)
+        maxlen = 0
+        maxFreq = 0
+        # iter thru all substr
+        for i in range(n):
+            for j in range(i,n):
+                count = Counter(s[i:j+1])
+                maxFreq = max(count.values())
+                if (j-i+1)-maxFreq <= k:
+                    maxlen = max(j-i+1, maxlen)
+                else:
+                    break
+        # create char freq
+        # if n-m <= k compare to max
+        # if n-m > k break and go to next start pos
+        # return max length
+        return maxlen
